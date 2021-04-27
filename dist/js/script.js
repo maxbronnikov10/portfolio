1b87a4d8490c8a77389b42a8eee4fae8e3bc7fa2
Maxim <maxbronnikov2004@gmail.com>
2021-04-28 02:47:43 +0300

    add menu and hamburger in first section

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.menu'),
+    hamburger = document.querySelector('.hamburger'),
+    close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
